e187f006ca890d88dae500e7ffebf892049a1000
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-05-16 21:31:44 -0500

    Refactor cart addition logic
    Code refactoring has been done to improve the logic of adding items to the cart. We removed the inline function call in the Guitar component and instead implemented a dedicated function for adding items to the cart in the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,29 @@ const App = () => {
     const [data, setData] = useState(db)
     const [cart, setCart] = useState([])
 
-  return (
-      <>
-          <Header/>
-          <main className="container-xl mt-5">
-              <h2 className="text-center">Nuestra Colección</h2>
+    const addToCart = (item) => {
+        setCart(prev => [...prev, item])
 
-              <div className="row mt-5">
-                  {data.map((item, index) => (<Guitar key={index} guitar={item} setCart={setCart}/>))}
-              </div>
-          </main>
-          <footer className="bg-dark mt-5 py-5">
-              <div className="container-xl">
-                  <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
-              </div>
-          </footer>
-      </>
-  )
+    }
+
+    return (
+        <>
+            <Header/>
+            <main className="container-xl mt-5">
+                <h2 className="text-center">Nuestra Colección</h2>
+
+                <div className="row mt-5">
+                    {data.map((item, index) => (
+                        <Guitar key={index} guitar={item} addToCart={addToCart}/>))}
+                </div>
+            </main>
+            <footer className="bg-dark mt-5 py-5">
+                <div className="container-xl">
+                    <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+                </div>
+            </footer>
+        </>
+    )
 };
 
 export default App
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,4 +1,4 @@
-export const Guitar = ({guitar, setCart}) => {
+export const Guitar = ({guitar, addToCart}) => {
     const {id, name, description, price, image} = guitar
 
     return (
@@ -14,7 +14,7 @@ export const Guitar = ({guitar, setCart}) => {
                     <button
                         type="button"
                         className="btn btn-dark w-100"
-                        onClick={() => setCart((prevcart) => [...prevcart, guitar])}
+                        onClick={() => addToCart(guitar)}
                     >Agregar al Carrito
                     </button>
                 </div>
